6eb91245b946e999a7e0cd58af1623995f17e671
ianperrin <ian.perrin@ntlworld.com>
2016-09-14 21:07:20 +0000

    First commit

---
+++ MMM-ModuleScheduler.js
@@ -0,0 +1,67 @@
+/* global Log, Module, moment, config, MM */
+/* Magic Mirror
+ * Module: MMM-ModuleScheduler
+ *
+ * By Ian Perrin http://ianperrin.com
+ * MIT Licensed.
+ */
+Module.register("MMM-ModuleScheduler",{
+	// Module config defaults.
+	defaults: {
+		schedulerClass: 'scheduler',
+		transitionInterval: 1000
+	},
+
+	// Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
+		this.sendSocketNotification('SET_CONFIG', this.config);
+	},
+	
+	notificationReceived: function(notification, payload, sender) {
+	    var self = this;
+		if (notification === 'DOM_OBJECTS_CREATED') {
+			MM.getModules().exceptModule(this).withClass(this.config.schedulerClass).enumerate(function(module) {
+				Log.log(self.name + ' wants to schedule the display of ' + module.name );
+				if (typeof module.config.module_schedule === 'object') {
+            		self.sendSocketNotification('SET_SCHEDULE_MODULE', {name: module.name, id: module.identifier, schedule: module.config.module_schedule});
+				} else {
+				    Log.error( module.name + ' is configured to be scheduled, but the module_schedule option is undefined' );
+				}
+			});
+		}
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+	    var self = this;
+	    
+		if (notification === 'SHOW_MODULE') {
+		    var identifier = payload;
+        	Log.log(self.name + ' received notification to show ' + identifier );
+			MM.getModules().exceptModule(this).withClass(this.config.schedulerClass).enumerate(function(module) {
+			    if (module.identifier === identifier){
+        			Log.log(self.name + ' is showing ' + module.name  + ' (' + module.identifier + ')' );
+        			module.show(self.config.transitionInterval, function() {
+            			Log.log(self.name + ' has shown ' + module.name + ' (' + module.identifier + ')');
+                    });
+        			return true;
+			    }
+			});
+		}
+
+		if (notification === 'HIDE_MODULE') {
+		    var identifier = payload;
+        	Log.log(self.name + ' received notification to hide ' + identifier );
+			MM.getModules().exceptModule(this).withClass(this.config.schedulerClass).enumerate(function(module) {
+			    if (module.identifier === identifier){
+        			Log.log(self.name + ' is hiding ' + module.name  + ' (' + module.identifier + ')' );
+        			module.hide(self.config.transitionInterval, function() {
+            			Log.log(self.name + ' has hidden ' + module.name + ' (' + module.identifier + ')');
+                    });
+        			return true;
+			    }
+			});
+		}
+
+	}	
+});
---
+++ node_helper.js
@@ -0,0 +1,82 @@
+/* Magic Mirror
+ * Node Helper: MMM-ModuleScheduler
+ *
+ * By Ian Perrin http://ianperrin.com
+ * MIT Licensed.
+ */
+
+var NodeHelper = require("node_helper");
+var CronJob = require("cron").CronJob;
+
+module.exports = NodeHelper.create({
+    cronJobs: [],
+
+    // Override start method.
+    start: function() {
+        console.log("Starting node helper for: " + this.name);
+    },
+
+    // Override socketNotificationReceived method.
+    socketNotificationReceived: function(notification, payload) {
+        console.log(this.name + ' received ' + notification);
+        var self = this;
+        
+        if (notification === "SET_CONFIG") {
+            console.log(this.name + ' is setting the config');
+            this.config = payload;
+            return true;
+        }
+        
+        if (notification === "SET_SCHEDULE_MODULE") {
+            module = payload;
+            if (!module.schedule.hasOwnProperty('from') || !module.schedule.hasOwnProperty('from')) {
+                console.log(this.name + ' cannot scheduling the module ' + module.name + ': invalid schedule');
+                return false;
+            }
+
+            console.log(this.name + ' is scheduling the module ' + module.name );
+
+            var showJob = this.createCronJobForModule(module, 'show');
+            var hideJob = this.createCronJobForModule(module, 'hide');
+
+            var now = new Date();
+            if (showJob.nextDate().toDate() > now && hideJob.nextDate().toDate() > now) {
+                this.sendSocketNotification('HIDE_MODULE', module.id);
+            }
+            console.log(this.name + ' has scheduled ' + module.name);
+            console.log(this.name + ' will next show ' + module.name + ' at ' + showJob.nextDate().toDate());
+            console.log(this.name + ' will next hide ' + module.name + ' at ' + hideJob.nextDate().toDate());
+
+            return true;
+            
+        }
+    },
+
+    createCronJobForModule: function(module, type) {
+        var self = this;
+        var moduleCronTime = (type === 'show' ? module.schedule.from : module.schedule.to );
+        
+        if(type !== 'show' && type !== 'hide') {
+            console.log(self.name + ' requires show/hide for type, not ' + type);
+            return false;
+        }
+        
+        try {
+            var job = new CronJob({
+                cronTime: moduleCronTime, 
+                onTick: function() {
+                    console.log(self.name + ' needs to ' + type + ' ' + module.name);
+                    self.sendSocketNotification(type.toUpperCase() + '_MODULE', module.id);
+                    console.log(self.name + ' will next ' + type + ' ' + module.name + ' at ' + this.nextDate().toDate());
+                }, 
+                onComplete: function() {
+                    console.log(self.name + ' has completed the ' + type + ' job');
+                }, 
+                start: true
+            });
+            return job;
+        } catch(ex) {
+            console.log(this.name + ' could not schedule ' + module.name + ' - invalid ' + type + ' expression: ' + moduleCronTime);
+        }
+    }
+});
